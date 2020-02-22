2e299f26fefec251618c16800ad6169f5f54faf6
sanchocovalho <54485238+sanchocovalho@users.noreply.github.com>
2020-02-22 07:52:57 +0300

    Add files via upload

--- car_catalog/settings.py
+++ car_catalog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ta_sqv-#t*j_(=f9b61rlutss1n%f701clg%$2#=n_22iw&y91'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
